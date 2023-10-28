adca8886003b6b8e664280718341d7a502d79362
OrosRuben79 <109885291+OrosRuben79@users.noreply.github.com>
2023-10-27 21:36:41 -0300

    agrego vista predeterminada en componente educacion

--- src/components/pages/components/Educacion.jsx
+++ src/components/pages/components/Educacion.jsx
@@ -14,21 +14,22 @@ const Educacion = () => {
           <div class="row">
             <div class="col-4">
               <div class="list-group" id="list-tab" role="tablist">
-              <a class="list-group-item list-group-item-action" id="list-settings-list" data-bs-toggle="list" href="#list-settings" role="tab" aria-controls="list-settings">Estudios Realizados</a>
+              <a class="list-group-item list-group-item-action active" id="list-settings-list" data-bs-toggle="list" href="#list-settings" role="tab" aria-controls="list-settings">Estudios Realizados</a>
                 <a class="list-group-item list-group-item-action" id="list-profile-list" data-bs-toggle="list" href="#list-profile" role="tab" aria-controls="list-profile">...</a>
                 <a class="list-group-item list-group-item-action" id="list-messages-list" data-bs-toggle="list" href="#list-messages" role="tab" aria-controls="list-messages">Cursos Realizados</a>
-                <a class="list-group-item list-group-item-action active" id="list-home-list" data-bs-toggle="list" href="#list-home" role="tab" aria-controls="list-home">...</a>
+                <a class="list-group-item list-group-item-action " id="list-home-list" data-bs-toggle="list" href="#list-home" role="tab" aria-controls="list-home">...</a>
               </div>
             </div>
             <div class="col-8">
               <div class="tab-content" id="nav-tabContent">
-              <div class="tab-pane fade" id="list-settings" role="tabpanel" aria-labelledby="list-settings-list">
+              <div class="tab-pane fade show active" id="list-settings" role="tabpanel" aria-labelledby="list-settings-list">
                 <p>• Full Stack Developer - Henry Student, 2023 - https://www.soyhenry.com/about-us</p>
                 <p>• Tècnico en Higiene y Seguridad Laboral - Universidad Tècnologica Nacional,  2022 http://www.frm.utn.edu.ar/index.php</p>
                 <p>• Programación desde 0 - Instituto online Egg, 2022 - https://moodle.eggeducacion.app/login/index.php</p>
                 <p></p>
                 ...</div>
                 <div class="tab-pane fade" id="list-profile" role="tabpanel" aria-labelledby="list-profile-list">...</div>
+                
                 <div class="tab-pane fade" id="list-messages" role="tabpanel" aria-labelledby="list-messages-list">
                 <p>• Creacion y Manejos de Dashboard en libros de excel- capacitaciòn online, 2019</p>
                   <p>• Analisis de Datos, Manejo de tablas dinàmicas y gràficos dinàmicos</p>
@@ -36,7 +37,7 @@ const Educacion = () => {
                   <p></p>
                   <p></p>
                   ...</div>
-                <div class="tab-pane fade show active" id="list-home" role="tabpanel" aria-labelledby="list-home-list">...</div>
+                <div class="tab-pane fade " id="list-home" role="tabpanel" aria-labelledby="list-home-list">...</div>
                 
               </div>
             </div>
